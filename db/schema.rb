4c3b26acce8f17007cffcbc89979876d2097ab1a
obanozin <obanozin18@gmail.com>
2019-10-14 07:15:03 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_044111) do
+ActiveRecord::Schema.define(version: 2019_10_14_071333) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "moto_infos", force: :cascade do |t|
     t.integer "user_id"
