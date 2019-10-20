1888a3adfe360799981f843e601ae47a76febd6d
obanozin <obanozin18@gmail.com>
2019-10-20 08:28:12 +0000

    [finish]touring

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -4,4 +4,5 @@ class TeamMember < ApplicationRecord
 	belongs_to :team
 	belongs_to :user
 	validates_uniqueness_of :user_id, scope: [:team_id]
+	scope :approval, -> { where(is_team: :approval)}
 end
