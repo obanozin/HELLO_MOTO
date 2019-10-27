a2889151f955fa316acfd3109d13c3af4e9738e7
obanozin <obanozin18@gmail.com>
2019-10-27 10:33:12 +0000

    dep

--- db/migrate/20191021064755_add_user_ref_and_touring_ref_to_message_tourings.rb
+++ db/migrate/20191021064755_add_user_ref_and_touring_ref_to_message_tourings.rb
@@ -1,7 +1,7 @@
 class AddUserRefAndTouringRefToMessageTourings < ActiveRecord::Migration[5.2]
   def change
-    add_reference :message_tourings, :user, foreign_key: true
-    add_reference :message_tourings, :touring, foreign_key: true
+    add_reference :message_tourings, :user, foreign_key: true,type: :integer
+    add_reference :message_tourings, :touring, foreign_key: true,type: :integer
     change_column_null :message_tourings, :user_id, false
     change_column_null :message_tourings, :touring_id, false
   end
