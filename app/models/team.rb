9d06c1d913e86ee0d95895adcc7e72160407058d
obanozin <obanozin18@gmail.com>
2019-10-29 08:14:28 +0000

    [fix]scss

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
 	belongs_to :user
 	attachment :team_image
-	has_many :team_members
+	has_many :team_members, dependent: :destroy
 	has_many :message_teams
 	has_many :tourings
 end
