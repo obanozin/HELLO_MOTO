fe779187ec505fca361acf12dd352d1a154d614b
obanozin <obanozin18@gmail.com>
2019-10-30 10:35:03 +0000

    [finish]all

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,6 @@ class Team < ApplicationRecord
 	belongs_to :user
 	attachment :team_image
 	has_many :team_members, dependent: :destroy
-	has_many :message_teams
+	has_many :message_teams, dependent: :destroy
 	has_many :tourings
 end
