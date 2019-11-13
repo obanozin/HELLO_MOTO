10269289431d7e26cbf04c2f33260f80a21f4207
obanozin <obanozin18@gmail.com>
2019-11-13 08:33:47 +0000

    [add]validates for teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,8 @@ class Team < ApplicationRecord
 	has_many :team_members, dependent: :destroy
 	has_many :message_teams, dependent: :destroy
 	has_many :tourings
+
+	validates :team_name,    length: { minimum: 1 }
+	validates :team_name,    length: { maximum: 20 }
+	validates :team_introduction,    length: { maximum: 200 }
 end
